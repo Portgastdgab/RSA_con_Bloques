1bef8e64061d7e5afe946d86aae72fc3fd2e7e97
Gabriel Ch <gabriel.choque@ucsp.edu.pe>
2021-07-05 11:35:41 -0500

    firma con txt

--- main.cpp
+++ main.cpp
@@ -8,8 +8,6 @@ int main() {
     RSA receptor(2048);
     RSA emisor(2048);
 
-    string msj="[Aaron Misash Apaza Coaquira(201-10-48240,77569358) , Gabriel Santiago Choque Mayta (201-10-48248,70809056) , Geraldo Mark Romero Guillen (201-10-48119,77956852) , Jean Carlo Gino Condori Gonzales (201-10-47536,78957018) , Jose Enrique Vilca Campana (201-10-48429,79632845) ]";
-
     //FIRMA
     //Emisor
     string firmaCifrada=emisor.firmaCipher(fileOpen("Signature.txt"),receptor.e,receptor.n);
@@ -24,10 +22,6 @@ int main() {
     receptor.decipher(emisor.crypted_letter);                 //El receptor decifra el mensaje
     emisor.show_encryption();                   //Muestra el cifrado almacenado en crypted_letter
     receptor.show_decryption();                 //Muestra el decifrado almacenado en message
-
-
-
-
 
     return 0;
 }
